f14b951afc9f5f992cf2bf4c6be0d3bb9706207e
thanhquang2004 <quangthanh7204@gmail.com>
2024-10-29 00:08:24 +0700

    Add auth service, authDto, loginDto and registerDto

--- src/main/java/com/example/task_manager/controller/AuthController.java
+++ src/main/java/com/example/task_manager/controller/AuthController.java
@@ -1,4 +1,4 @@
 package com.example.task_manager.controller;
 
-public class UserController {
+public class AuthController {
 }
--- src/main/java/com/example/task_manager/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.example.task_manager.service;
-
-public class UserService {
-}
