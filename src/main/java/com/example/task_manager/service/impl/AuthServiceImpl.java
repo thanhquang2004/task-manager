6920ae6d8dd3a7bd7890736462192c454a4f3675
thanhquang2004 <quangthanh7204@gmail.com>
2024-10-30 15:52:06 +0700

    Create JWTService

--- src/main/java/com/example/task_manager/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/task_manager/service/impl/AuthServiceImpl.java
@@ -3,9 +3,29 @@ package com.example.task_manager.service.impl;
 import com.example.task_manager.dto.request.AuthDto;
 import com.example.task_manager.dto.request.LoginDto;
 import com.example.task_manager.dto.request.RegisterDto;
+import com.example.task_manager.repository.RoleRepository;
+import com.example.task_manager.repository.UserRepository;
 import com.example.task_manager.service.AuthService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthServiceImpl implements AuthService {
+    UserRepository userRepository;
+    RoleRepository roleRepository;
+    PasswordEncoder passwordEncoder;
+    AuthenticationManager authenticationManager;
+
+
+
     @Override
     public String register(RegisterDto registerDto) {
         return null;
@@ -13,7 +33,10 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthDto login(LoginDto loginDto) {
-        return null;
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword())
+        );
+
     }
 
     @Override
